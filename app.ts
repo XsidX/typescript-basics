8ae60abc62c56e52e1bed4ad87283a16855b5bc2
iamsid <developer.sid@outlook.com>
2021-05-31 15:27:17 +0300

    Types--> any

--- app.ts
+++ app.ts
@@ -4,13 +4,16 @@
 // enum Role { ADMIN, READ_ONLY, AUTHOR }//or
 enum Role { ADMIN = 5, READ_ONLY, AUTHOR }
 
-const person = {   //typescript infers what the object type is defined as
+const person: {
+    name: string,
+    age: number,
+    hobbies: any,
+    
+} = {   //typescript infers what the object type is defined as
     name: 'sid',
     age: 30,
     hobbies: ['sports', 'cooking'],
-    role: Role.ADMIN
+    
 }
 
-if (person.role === Role.ADMIN){
-    console.log('is Admin')
-}
+person.hobbies = 2 // any type is allowed avoid whenever possible
