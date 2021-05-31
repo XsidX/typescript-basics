ef7de097a79d25ced1c4d9a102a660136d9b66df
iamsid <developer.sid@outlook.com>
2021-05-31 13:42:03 +0300

    Using types

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log('your app goes here...');
+//core data types
+function add(n1, n2) {
+    return n1 + n2;
+}
+var number1 = '5'; //something went wrong here
+var number2 = 2.85;
+var result = add(number1, number2);
+console.log(result);
--- app.ts
+++ app.ts
@@ -1,1 +1,10 @@
-console.log('your app goes here...')
+//core data types
+
+function add(n1: number, n2: number) {
+    return n1 + n2
+}
+const number1 = '5'  //something went wrong here
+const number2 = 2.85
+const result = add(number1, number2)
+
+console.log(result)
