02b09a6f017f1ff9e56c243d8190feed7b477fe6
iamsid <developer.sid@outlook.com>
2021-05-31 15:22:14 +0300

    Types--> Enums

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
-//core data types
-function add(n1, n2, showResult, phrase) {
-    return n1 + n2;
+// //tUPLES----arrays of fixed length
+// person.role[1] = 10 //error caught
+// person.role.push('admin') //error not caught coz push is an exeption
+// person.role = [0, 'admin', 'something else'] //error caught
+// // let favouriteActivities: string[]
+// // favouriteActivities = ['sports', 1]
+// // let favouriteActivities: any[]
+// // favouriteActivities = ['sports', 1]
+// // console.log(person.name) //typescript infers what the object type is defined as
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["RAED_ONLY"] = 1] = "RAED_ONLY";
+    Role[Role["AUTHOR"] = 2] = "AUTHOR";
+})(Role || (Role = {}));
+var person = {
+    name: 'sid',
+    age: 30,
+    hobbies: ['sports', 'cooking'],
+    role: Role.ADMIN
+};
+if (person.role === Role.ADMIN) {
+    console.log('is Admin');
 }
-var number1 = 5; //something went wrong here
-var number2 = 2.85;
-var printResult = true;
-var resultPhrase = 'Result id:';
-var result = add(number1, number2, printResult, resultPhrase);
-console.log(result);
--- app.ts
+++ app.ts
@@ -1,29 +1,16 @@
-//tUPLES----arrays of fixed length
 
 
-const person: {
-    name: string
-    age: number
-    hobbies: string[]
-    role: [number, string] //tuple
-} = {   //typescript infers what the object type is defined as
+
+// enum Role { ADMIN, READ_ONLY, AUTHOR }//or
+enum Role { ADMIN = 5, READ_ONLY, AUTHOR }
+
+const person = {   //typescript infers what the object type is defined as
     name: 'sid',
     age: 30,
     hobbies: ['sports', 'cooking'],
-    role: [2, 'author']
+    role: Role.ADMIN
 }
 
-person.role[1] = 10 //error caught
-person.role.push('admin') //error not caught coz push is an exeption
-person.role = [0, 'admin', 'something else'] //error caught
-
-// let favouriteActivities: string[]
-
-// favouriteActivities = ['sports', 1]
-
-// let favouriteActivities: any[]
-
-// favouriteActivities = ['sports', 1]
-
-// console.log(person.name) //typescript infers what the object type is defined as
-
+if (person.role === Role.ADMIN){
+    console.log('is Admin')
+}
