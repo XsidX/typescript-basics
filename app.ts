ecd4232f61b60cc75c3580d011202cf6f1d1ec72
iamsid <developer.sid@outlook.com>
2021-05-31 16:47:27 +0300

    ReturnTypes & Void

--- app.ts
+++ app.ts
@@ -1,36 +1,10 @@
-//Union type
-type Combinable = number | string
-type ConversionDescriptor = 'as-number' | 'as-text'
-
-function combine(
-    input1: Combinable,
-    input2: Combinable,
-    resultConversion: ConversionDescriptor
-    
-) {
-    
-    let result
-    if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number'){
-    result = +input1 + +input2
-    }else {
-        result = input1.toString() + input2.toString()
-    }
-
-    return result
-    // if (resultConversion === 'as-number'){
-    //     return +result
-    // }else{
-    //     return result.toString()
-    // }
-   
+function add(n1: number, n2: number): number{ //let typescript infer the type if u have no reason to do this
+    return n1 + n2
 }
 
-const combinedAges = combine(30, 26, 'as-number')
-console.log(combinedAges)
+function printResult(num: number): void { //return type void
+    console.log('Result: ' + num)
+}
 
-const combinedStringAges = combine('30', '26', 'as-number')
-console.log(combinedStringAges)
+console.log(printResult(add(5, 12))) //returns undefined since printResult does not return anything
 
-
-const combinedNames = combine('Max', 'Anna', 'as-text')
-console.log(combinedNames)
