56a40b05d23da0d8c5e9db6f031679c11d9763c5
iamsid <developer.sid@outlook.com>
2021-05-31 17:06:34 +0300

    Function Types & Callbacks

--- app.ts
+++ app.ts
@@ -6,15 +6,25 @@ function printResult(num: number): void { //return type void
     console.log('Result: ' + num)
 }
 
-console.log(printResult(add(5, 12))) //returns undefined since printResult does not return anything
-
-// let combineValues: Function
-//combineValues = printResult //error not caught but is there
-let combineValues: (a: number, b: number) => number
-
-combineValues = add
-// combineValues = 5 //error caught
-combineValues = printResult //error caught
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void){
+    const result = n1 + n2
+    cb(result)
+}
 
 
-console.log(combineValues(8, 8))
+addAndHandle(10, 20, (result) => {
+    console.log(result)
+})
+
+// console.log(printResult(add(5, 12))) //returns undefined since printResult does not return anything
+
+// // let combineValues: Function
+// //combineValues = printResult //error not caught but is there
+// let combineValues: (a: number, b: number) => number
+
+// combineValues = add
+// // combineValues = 5 //error caught
+// // combineValues = printResult //error caught
+
+
+// console.log(combineValues(8, 8))
