78642f4926cd10f6a1d286c92b72bf93081922a6
iamsid <developer.sid@outlook.com>
2021-05-31 14:24:13 +0300

    Types-->numbers, strings, boolean

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 //core data types
-function add(n1, n2) {
+function add(n1, n2, showResult, phrase) {
     return n1 + n2;
 }
-var number1 = '5'; //something went wrong here
+var number1 = 5; //something went wrong here
 var number2 = 2.85;
-var result = add(number1, number2);
+var printResult = true;
+var resultPhrase = 'Result id:';
+var result = add(number1, number2, printResult, resultPhrase);
 console.log(result);
--- app.ts
+++ app.ts
@@ -1,10 +1,12 @@
 //core data types
 
-function add(n1: number, n2: number) {
+function add(n1: number, n2: number, showResult: boolean, phrase: string) {
     return n1 + n2
 }
-const number1 = '5'  //something went wrong here
+const number1 = 5  //something went wrong here
 const number2 = 2.85
-const result = add(number1, number2)
+const printResult = true
+const resultPhrase = 'Result id:'
+const result = add(number1, number2, printResult, resultPhrase)
 
 console.log(result)
